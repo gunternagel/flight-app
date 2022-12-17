5b4b37f7690f27a15fa5ec6690236c9a4dbacd38
Gunter Nagel <gunter.nagel@gmx.de>
2022-12-17 21:18:38 +0100

    Einige Aenderungen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
+
 
 @NgModule({
    imports: [
--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { Flight } from '../flight';
 
 @Component({
   selector: 'app-flight-search',
@@ -9,6 +12,35 @@ export class FlightSearchComponent implements OnInit{
 
   from = 'Hamburg';
   to = 'Graz';
-  flights: Array<Flight>;
+  flights: Array<Flight> = [];
   selectedFlight: Flight | null = null;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    // this is empty
+  }
+
+  search() :void {
+
+    const url = 'http://demo.ANGULARarchitects.io/api/flight';
+
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    const params = new HttpParams().set('from', this.from).set('to', this.to);
+
+    this.http.get<Flight[]>(url, {headers, params}).subscribe({
+      next: (flights) => {
+        this.flights = flights;
+      },
+      error: (err) => {
+        console.error('Error', err);
+      }
+    });
+  }
+
+  select(f: Flight) {
+    this.selectedFlight = f;
+  }
 }
